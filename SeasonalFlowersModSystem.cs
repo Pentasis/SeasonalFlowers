cbc890b67c1780904e2ffa8fa30ea0f8acdb6061
Pentasis <7046024+Pentasis@users.noreply.github.com>
2025-12-11 14:33:05 +0100

    Initial commit

---
+++ SeasonalFlowersModSystem.cs
@@ -0,0 +1,13 @@
+﻿using Vintagestory.API.Common;
+
+[assembly: ModInfo("SeasonalFlowers", "SeasonalFlowers",
+    Authors = new string[] { "Unknown" },
+    Description = "This is a sample mod",
+    Version = "1.0.0")]
+
+namespace SeasonalFlowers
+{
+    public class SeasonalFlowersModSystem : ModSystem
+    {
+    }
+}
